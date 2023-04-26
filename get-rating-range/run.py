1a21875a5dcdb7225daca56ed22260cc5a6ec321
Caleb Courtney <caleb-courtney@pluralsight.com>
2023-04-25 17:47:08 -0700

    add average and remove keyboard interrupt

--- get-rating-range/run.py
+++ get-rating-range/run.py
@@ -1,11 +1,6 @@
 """
-This script will take a PGN file
-and counts the number of games played at each ELO in the file
-
-The output is a bar graph of number of games played
-in each ELO bucket broken into 10 buckets
-
-Output is expectedly a fairly normal distribution
+This script will read a PGN file passed in from the command line
+and gets the max, min, and average ELO of the players that played that month
 """
 
 import sys
@@ -18,21 +13,25 @@ elo_pattern = re.compile(r'\[(White|Black)Elo "(\d+)"\]')
 
 min_elo = 1_000_000
 max_elo = -1
+avg_elo = 0
+count = 0
 
-try:
-    for line in tqdm(sys.stdin):
-        if line.startswith('[WhiteElo "'):
-            white_elo = int(elo_pattern.match(line).group(2))
-            min_elo = min(min_elo, white_elo)
-            max_elo = max(max_elo, white_elo)
+for line in tqdm(sys.stdin):
+    if line.startswith('[WhiteElo "'):
+        white_elo = int(elo_pattern.match(line).group(2))
+        min_elo = min(min_elo, white_elo)
+        max_elo = max(max_elo, white_elo)
+        avg_elo = ((avg_elo * (1 if count == 0 else count)) + white_elo) / (count + 1)
+        count += 1
 
-        elif line.startswith('[BlackElo "'):
-            black_elo = int(elo_pattern.match(line).group(2))
-            min_elo = min(min_elo, white_elo)
-            max_elo = max(max_elo, white_elo)
+    elif line.startswith('[BlackElo "'):
+        black_elo = int(elo_pattern.match(line).group(2))
+        min_elo = min(min_elo, white_elo)
+        max_elo = max(max_elo, white_elo)
+        avg_elo = ((avg_elo * (1 if count == 0 else count)) + black_elo) / (count + 1)
+        count += 1
 
-except KeyboardInterrupt:
-    pass
 
 print(f"Min ELO: {min_elo}")
 print(f"Max ELO: {max_elo}")
+print(f"Avg ELO: {round(avg_elo)}")
